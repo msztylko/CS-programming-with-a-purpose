bf5ef02bb9f02056fc563d1216a659fe057d3a37
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-03-28 10:23:13 +0200

    add SAT

---
+++ w05-functions/extra/SAT.java
@@ -0,0 +1,7 @@
+public class SAT {
+    public static void main(String[] args) {
+        double z = Double.parseDouble(args[0]);
+        double v = Gaussian.cdf((z - 1019) / 209);
+        StdOut.println(v);
+    }
+}
